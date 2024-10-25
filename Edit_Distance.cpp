315cb7f264134b8e2bc275777431a7b5d584c922
Rishith <rishithrishith1@gmail.com>
2024-10-25 14:20:31 +0530

    new files

--- Edit_Distance.cpp
+++ Edit_Distance.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int edit_distance(string a,string b){
     int x=a.size();
     int y=b.size();
-    vector<vector<int>>ar(a+1,vector<int>(b+1));
-    for(int i=0;i<=a;i++){
-        for(int j=0;j<=b;j++){
+    vector<vector<int>>ar(x+1,vector<int>(y+1));
+    for(int i=0;i<=x;i++){
+        for(int j=0;j<=y;j++){
             if(i==0){
                 ar[i][j]=j;
             }
@@ -16,11 +16,11 @@ int edit_distance(string a,string b){
                 ar[i][j]=ar[i-1][j-1];
             }
             else{
-                ar[i][j]=1+min(ar[i-1][j],ar[i][j-1],arr[i-1][j-1]);
+                ar[i][j]=1+min({ar[i-1][j],ar[i][j-1],ar[i-1][j-1]});
             }
         }
     }
-    
+    return ar[x][y];
 }
 int main(){
     string a,b;
@@ -29,5 +29,4 @@ int main(){
     cout<<"Enter string 2: ";
     getline(cin,b);
     cout<<edit_distance(a,b);
-
 }
